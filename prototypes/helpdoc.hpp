5441f6c323067a1256addcdf928caaa755553d90
FireC657 <comptagedepoints@gmail.com>
2023-04-19 20:41:10 +0100

    Help document header added

---
+++ prototypes/helpdoc.hpp
@@ -0,0 +1,5 @@
+#ifndef __HELP_DOC_INCLUDED 
+#define __HELP_DOC_INCLUDED 
+#include <string>
+std::string* inithelp();
+#endif
---
+++ src/helpdoc.cpp
@@ -0,0 +1,8 @@
+#include "../prototypes/helpdoc.hpp"
+#include <string>
+
+std::string* inithelp(){
+    std::string* hlp = new std::string("");
+    *hlp = "--ignore-comments:        Ignores comments # in the source file.\n--ignore-halts:           Ignores ! known as halts.\n--unsignal-halt:          Treats a halt as program ending without signaling.\n--prg-settings:           Displays the setup settings in the program (eg: Memory allocated).\n--count-instructions:     Counts the instructions that have been executed.\n--license | -l | -lc:     Displays the GNU General public license.\n\n(Syntax-Highlighting)\n\n--syntax-highlight:       Switchs to the syntax highlighter.\n--disable-comments:       Hides the comment.\n--disable-commenthash:    Hides the \'#\' but still shows the comment.\n--line-number:            Shows the current line.\n"; 
+    return hlp;
+}
