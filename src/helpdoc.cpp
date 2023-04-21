3c916f0f76f936c228b0cebca454929c4fd4df3e
FireC657 <comptagedepoints@gmail.com>
2023-04-21 16:02:45 +0100

    Fixed Documentation + Memory dumping issues fixed

--- src/helpdoc.cpp
+++ src/helpdoc.cpp
@@ -4,19 +4,21 @@
 std::string* inithelp(){
     std::string* hlp = new std::string("");
     *hlp = 
-    "--ignore-comments:          Ignores comments \'#\' in the source file."
+    "--ignore-comments:        Ignores comments \'#\' in the source file."
     "\n--ignore-halts:           Ignores \'!\' known as halts."
     "\n--unsignal-halt:          Treats a halt as program ending without signaling."
     "\n--prg-settings:           Displays the setup settings in the program (eg: Memory allocated)."
     "\n--count-instructions:     Counts the instructions that have been executed."
     "\n--license | -l | -lc:     Displays the GNU General public license."
     "\n\n(Syntax-Highlighting)\n\n"
-    "--syntax-highlight:         Switchs to the syntax highlighter."
-    "\n--disable-comments:       Hides the comment."
+    "--syntax-highlight:       Switchs to the syntax highlighter.\n"
+    " ~~~~~~~~~~~~~~~~^ -> \033[33m\033[1mNOTE: All other options concering the syntax-highlighter must come before the command itself.\033[0m\n"
+    "--disable-comments:       Hides the comment."
     "\n--disable-commenthash:    Hides the \'#\' but still shows the comment."
     "\n--line-number:            Shows the current line.\n"
+    "\n(Misc Commands)"
     "\n--precision-halt:         Shows the exact place where the halt happend.\n"
     "\n--memory-dump:            Shows the current state of memory at the end of the program.\n"
-    "  ~~~~~~~~~~~~~^ -> NOTE: The Yellow-Colored byte is the current location of the pointer.\n";
+    "  ~~~~~~~~~~~^ -> \033[33m\033[1mNOTE: The Yellow-Colored byte is the current location of the pointer.\033[0m\n";
     return hlp;
 }
