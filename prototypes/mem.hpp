1df4436845ad2abe9380618563f4a3855436e204
FireC657 <comptagedepoints@gmail.com>
2023-05-04 21:32:59 +0100

    Removed unused files

--- prototypes/mem.hpp
+++ prototypes/mem.hpp
@@ -1,6 +1,5 @@
 #ifndef MEM_INCLUDED_HPP
 #define MEM_INCLUDED_HPP 
-#include <vector>
 
 //typedef unsigned char unsigned char;
 struct mem_snapshot{
@@ -8,16 +7,5 @@ struct mem_snapshot{
     int current_loc;
     int memory_len;
 };
-
-//mem_snapshot* save_snapshot(unsigned char* m, int ptr, int len);
-
-
-// Creates a memory snapshot at point 't', allocates it onto the heap, then push it onto the snapshot holder.
-mem_snapshot* push_snapshot
-     (unsigned char* m, const int& cptr, const int& mlen);
-
-// Automatically, clears the snapshot holder by deleting & freeing up memory
-//void clear_snapshotholder(std::vector<mem_snapshot*>& snapshot_holder);
-
 
 #endif
